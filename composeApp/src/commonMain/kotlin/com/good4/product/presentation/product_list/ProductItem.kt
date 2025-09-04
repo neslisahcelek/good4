272791e06e71d48450628dc122cbd3bdd1903320
Neslişah Çelek <neslisah.celek@outlook.com>
2025-09-04 19:58:26 +0300

    Simplify ProductItem: remove invalid painter usage, show basic texts

--- composeApp/src/commonMain/kotlin/com/good4/product/presentation/product_list/ProductItem.kt
+++ composeApp/src/commonMain/kotlin/com/good4/product/presentation/product_list/ProductItem.kt
@@ -1,39 +1,37 @@
 package com.good4.product.presentation.product_list
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.good4.product.Product
-import org.jetbrains.compose.resources.DrawableResource
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import com.good4.product.Product
 
 @Composable
 fun ProductItem(
     product: Product,
     modifier: Modifier = Modifier
 ) {
-    Column(
-
-    ) {
-        Icon(
-            painter = painterResource(DrawableResource(id = product.imageUrl)),
-        )
+    Column(modifier = modifier) {
+        Text(text = product.name, style = MaterialTheme.typography.h6)
+        Text(text = product.description, style = MaterialTheme.typography.body2)
+        Text(text = product.price, style = MaterialTheme.typography.subtitle1)
     }
 }
 
 @Preview
 @Composable
 fun ProductItemPreview(modifier: Modifier = Modifier) {
-    MaterialTheme{
-        ProductItem(product = Product(
-            id = 1,
-            name = "Kahve",
-            description = "description",
-            price = "price",
-            imageUrl = "imageUrl"
-        ))
+    MaterialTheme {
+        ProductItem(
+            product = Product(
+                id = 1,
+                name = "Kahve",
+                description = "description",
+                price = "price",
+                imageUrl = "imageUrl"
+            )
+        )
     }
 }
