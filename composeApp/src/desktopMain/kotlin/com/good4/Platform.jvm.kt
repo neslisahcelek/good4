c153989f50176e2fdfe1002c0a8deee96968cb0e
Neslişah Çelek <neslisah.celek@outlook.com>
2025-09-25 20:34:21 +0300

    platform files deleted

--- composeApp/src/desktopMain/kotlin/com/good4/Platform.jvm.kt
+++
@@ -1,8 +0,0 @@
-package com.good4
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
-
